a17e04f664acab17308f1a65a6a77a44b1c25182
Spec-75 <Spec_75@mail.ru>
2022-09-11 17:58:35 +0300

    add programm

---
+++ final test/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите количество элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] getArray = new string[size];
+string[] newArray = new string[getArray.Length];
+for (int i = 0; i < getArray.Length; i++)
+    {
+        Console.WriteLine("Введите элемент: ");
+        getArray[i] = Console.ReadLine();
+        if (getArray[i].Length <= 3)
+        {
+        newArray[i] = getArray[i];       
+        }
+    }
+
+Console.WriteLine($"Исходный массив: ");
+Console.WriteLine(String.Join(" ", getArray));
+
+Console.WriteLine("Обработанный массив: ");
+Console.WriteLine(String.Join(" ", newArray));
+
